0d861ecdcc572dddd938efdff872654459650248
Arteau <arteaudemeester@gmail.com>
2016-10-06 15:30:08 +0200

    added oplossing comments

---
+++ opdracht-comments/oplossing-comments-deel1.php
@@ -0,0 +1,19 @@
+<?php
+    
+/*
+    naam =  Arteau De Meester
+    email = arteaudemeester@gmail.com
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Oplossing Comments: deel 1</title>
+</head>
+<body>
+    
+</body>
+</html>
